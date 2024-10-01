19abf369a22e84cae31682c60341c2ede8f132b7
jp-hidalgo <jp.hidalgo@uniandes.edu.co>
2024-10-01 15:21:24 -0500

    merge

--- flutter_application_1/lib/app.dart
+++ flutter_application_1/lib/app.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/features/authentication/views/detail_view.dart';
+import 'package:flutter_application_1/features/authentication/views/list_items_view.dart';
+import 'package:flutter_application_1/features/authentication/views/login_view.dart';
+import 'package:flutter_application_1/features/authentication/views/profile_view.dart';
+import 'package:flutter_application_1/features/authentication/views/register_view.dart';
 import 'package:flutter_application_1/features/personalization/screens/sustainability/sustainability.dart';
 import 'package:flutter_application_1/features/shop/screens/cart/cart.dart';
-import 'package:flutter_application_1/utils/theme/theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,50 +13,24 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-      home: HomeScreen(),
+      title: 'EcoStyle',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/login', // Inicia en la vista de login
+      routes: {
+        '/login': (context) => LoginView(),
+        '/register': (context) => RegisterView(),
+        '/profile': (context) => ProfileView(),
+        '/detail': (context) => DetailView(),
+        '/list': (context) => ListItemsView(),
+        '/sustainability' : (context) => Sustainability(),
+        '/cart': (context) => CartScreen()
+      },
     );
   }
 
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) =>  CartScreen()),
-                );
-              },
-              child: Text('Cart'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Sustainability()),
-                );
-              },
-              child: Text('Sustainability'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
