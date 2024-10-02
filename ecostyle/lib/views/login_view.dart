08bcec9579a11335af0f2102ffe3ef820b8e21a2
David Leon <da.leon@uniandes.edu.co>
2024-10-01 23:30:35 -0500

    change password view
    se crea change password, para cambiarla te pide FaceID

--- ecostyle/lib/views/login_view.dart
+++ ecostyle/lib/views/login_view.dart
@@ -1,64 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 
 class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  Future<void> _authenticateWithBiometrics(BuildContext context) async {
+    final LocalAuthentication auth = LocalAuthentication();
+
+    try {
+      bool authenticated = await auth.authenticate(
+        localizedReason: 'Please authenticate to log in',
+        options: const AuthenticationOptions(
+          useErrorDialogs: true,
+          stickyAuth: true,
+        ),
+      );
+
+      if (authenticated) {
+        // Si la autenticación es exitosa, navega a la vista del perfil
+        Navigator.pushNamed(context, '/profile');
+      } else {
+        // Manejar el caso donde la autenticación falla
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Authentication failed')),
+        );
+      }
+    } catch (e) {
+      // Manejar excepciones
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error authenticating')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF012826), // Fondo
+      backgroundColor: const Color(0xFF012826), // Fondo
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'EcoStyle',
               style: TextStyle(fontSize: 32, color: Colors.white), // Título
             ),
-            SizedBox(height: 40),
+            const SizedBox(height: 40),
             TextField(
               decoration: InputDecoration(
                 labelText: 'Email',
-                labelStyle: TextStyle(color: Colors.white),
+                labelStyle: const TextStyle(color: Colors.white),
                 filled: true,
-                fillColor: Color(0xFF007451), // Cambiado a un verde más claro
+                fillColor: const Color(0xFF007451), // Cambiado a un verde más claro
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(8), // Ajustar los bordes redondeados
+                  borderSide: BorderSide.none, // Sin borde visible
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             TextField(
-              obscureText: true,
+              obscureText: true, // Para ocultar la contraseña
               decoration: InputDecoration(
                 labelText: 'Password',
-                labelStyle: TextStyle(color: Colors.white),
+                labelStyle: const TextStyle(color: Colors.white),
                 filled: true,
-                fillColor: Color(0xFF007451), // Cambiado a un verde más claro
+                fillColor: const Color(0xFF007451), // Cambiado a un verde más claro
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
                   borderSide: BorderSide.none,
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF007451), // Color del botón
+                backgroundColor: const Color(0xFF007451), // Color del botón
               ),
               onPressed: () {
                 // Lógica para iniciar sesión
                 Navigator.pushNamed(context, '/profile');
               },
-              child: Text('Login', style: TextStyle(color: Colors.white)), // Color del texto
+              child: const Text('Login', style: TextStyle(color: Colors.white)), // Color del texto
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF007451), // Color del botón
+              ),
+              onPressed: () {
+                _authenticateWithBiometrics(context); // Llama a la autenticación biométrica
+              },
+              child: const Text('Login with Biometrics', style: TextStyle(color: Colors.white)), // Color del texto
+            ),
+            const SizedBox(height: 10),
             TextButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/register');
               },
-              child: Text(
-                'Dont have an account? Sign Up',
+              child: const Text(
+                'Don\'t have an account? Sign Up',
                 style: TextStyle(color: Colors.white), // Color del texto
               ),
             ),
--- ecostyle/lib/views/register_view.dart
+++ ecostyle/lib/views/register_view.dart
@@ -1,102 +1,104 @@
 import 'package:flutter/material.dart';
 
 class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF012826), // Fondo
+      backgroundColor: const Color(0xFF012826), // Fondo
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Sign Up!',
               style: TextStyle(fontSize: 32, color: Colors.white), // Título
             ),
-            SizedBox(height: 40),
+            const SizedBox(height: 40),
             TextField(
               decoration: InputDecoration(
                 labelText: 'Email',
-                labelStyle: TextStyle(color: Colors.white),
+                labelStyle: const TextStyle(color: Colors.white),
                 filled: true,
-                fillColor: Color(0xFF007451), // Verde más claro
+                fillColor: const Color(0xFF007451), // Verde más claro
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
                   borderSide: BorderSide.none,
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             TextField(
               obscureText: true,
               decoration: InputDecoration(
                 labelText: 'Password',
-                labelStyle: TextStyle(color: Colors.white),
+                labelStyle: const TextStyle(color: Colors.white),
                 filled: true,
-                fillColor: Color(0xFF007451), // Verde más claro
+                fillColor: const Color(0xFF007451), // Verde más claro
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
                   borderSide: BorderSide.none,
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             TextField(
               decoration: InputDecoration(
                 labelText: 'Name',
-                labelStyle: TextStyle(color: Colors.white),
+                labelStyle: const TextStyle(color: Colors.white),
                 filled: true,
-                fillColor: Color(0xFF007451), // Verde más claro
+                fillColor: const Color(0xFF007451), // Verde más claro
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
                   borderSide: BorderSide.none,
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             TextField(
               decoration: InputDecoration(
                 labelText: 'Address',
-                labelStyle: TextStyle(color: Colors.white),
+                labelStyle: const TextStyle(color: Colors.white),
                 filled: true,
-                fillColor: Color(0xFF007451), // Verde más claro
+                fillColor: const Color(0xFF007451), // Verde más claro
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
                   borderSide: BorderSide.none,
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             TextField(
               decoration: InputDecoration(
                 labelText: 'Phone',
-                labelStyle: TextStyle(color: Colors.white),
+                labelStyle: const TextStyle(color: Colors.white),
                 filled: true,
-                fillColor: Color(0xFF007451), // Verde más claro
+                fillColor: const Color(0xFF007451), // Verde más claro
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8),
                   borderSide: BorderSide.none,
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF007451), // Color del botón
+                backgroundColor: const Color(0xFF007451), // Color del botón
               ),
               onPressed: () {
                 // Lógica para registrar
                 Navigator.pushNamed(context, '/profile');
               },
-              child: Text('Register', style: TextStyle(color: Colors.white)), // Color del texto
+              child: const Text('Register', style: TextStyle(color: Colors.white)), // Color del texto
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             TextButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/login'); // Redirigir a login
               },
-              child: Text(
+              child: const Text(
                 'Already have an account? Login',
                 style: TextStyle(color: Colors.white), // Color del texto
               ),
