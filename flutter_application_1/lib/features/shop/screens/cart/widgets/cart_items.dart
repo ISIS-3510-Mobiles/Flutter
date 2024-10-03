864a84c862fbf839a8a9b4eb8ca4caec2c407973
jp-hidalgo <jp.hidalgo@uniandes.edu.co>
2024-10-02 21:57:28 -0500

    cart items

--- flutter_application_1/lib/features/shop/screens/cart/widgets/cart_items.dart
+++ flutter_application_1/lib/features/shop/screens/cart/widgets/cart_items.dart
@@ -1,39 +1,44 @@
 import 'package:flutter/material.dart';
-
 import '../cart.dart';
 
 class TCartItems extends StatelessWidget {
   const TCartItems({
-    super.key, this.showButtons = true,
+    super.key,
+    this.showButtons = true,
   });
 
-  final  bool showButtons;
+  final bool showButtons;
 
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
       shrinkWrap: true,
-      separatorBuilder: (_, __) => const SizedBox(height: 32.0,),
+      separatorBuilder: (_, __) => const SizedBox(height: 32.0),
       itemCount: 2,
-      itemBuilder: (_, index)=> Column(
+      itemBuilder: (_, index) => Column(
         children: [
-          const CartItem(),
-          if (showButtons) const SizedBox(width: 12.0,),
+          const TCartItems(),
+          if (showButtons) const SizedBox(width: 12.0),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween ,
-            /// +/- buttons
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              if(showButtons) const Row(
-                children: [
-                  SizedBox( width: 70,),
-                  TAddRemoveQuantityButton(),
-                ],
+              if (showButtons)
+                Row(
+                  children: const [
+                    SizedBox(width: 12.0), // Added space before +/- buttons
+                    TAddRemoveQuantityButton(),
+                  ],
+                ),
+              const SizedBox(width: 12.0),
+              const Text(
+                '\$115',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              const SizedBox( width: 70,),
-              const Text('\$115')
-
             ],
-          )
+          ),
         ],
       ),
     );
