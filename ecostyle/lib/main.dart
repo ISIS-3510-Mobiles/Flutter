86c0824ffa51ab0425d4b63c5906535ceda796e2
Juan Vasquez <jd.vasquezh@uniandes.edu.co>
2024-10-02 23:56:32 -0500

    Small change to main.dart (not uploaded)

--- ecostyle/lib/main.dart
+++ ecostyle/lib/main.dart
@@ -5,13 +5,15 @@ import 'views/login_view.dart';
 import 'views/register_view.dart';
 import 'views/profile_view.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // Usa la configuración correcta
+  );
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
