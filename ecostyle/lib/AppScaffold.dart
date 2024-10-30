27e552d8cda2c5a66262745668bbbe1b7d5671f7
David Leon <da.leon@uniandes.edu.co>
2024-10-30 01:27:30 -0500

    Se agrega boton de 'Log Out' en el Menu Ecostyle

--- ecostyle/lib/AppScaffold.dart
+++ ecostyle/lib/AppScaffold.dart
@@ -19,53 +19,68 @@ class AppScaffold extends StatelessWidget {
       appBar: AppBar(
         title: const Text('EcoStyle'),
       ),
-      drawer: showDrawer ? Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color(0xFF012826),
+      drawer: showDrawer
+          ? Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  const DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF012826),
+                    ),
+                    child: Text(
+                      'EcoStyle Menu',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.person),
+                    title: const Text('Profile'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/profile');
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.list),
+                    title: const Text('List Items'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/list');
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.shopping_cart),
+                    title: const Text('Cart'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/cart');
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.eco),
+                    title: const Text('Sustainability'),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/sustainability');
+                    },
+                  ),
+                  const Divider(), // Add a divider for better visual separation
+                  ListTile(
+                    leading: const Icon(Icons.logout),
+                    title: const Text('Log Out'),
+                    onTap: () {
+                      // Navigate to the login view
+                      Navigator.pushNamedAndRemoveUntil(
+                        context,
+                        '/login',
+                        (Route<dynamic> route) => false,
+                      );
+                    },
+                  ),
+                ],
               ),
-              child: Text(
-                'EcoStyle Menu',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('Profile'),
-              onTap: () {
-                Navigator.pushNamed(context, '/profile');
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.list),
-              title: const Text('List Items'),
-              onTap: () {
-                Navigator.pushNamed(context, '/list');
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.shopping_cart),
-              title: const Text('Cart'),
-              onTap: () {
-                Navigator.pushNamed(context, '/cart');
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.eco),
-              title: const Text('Sustainability'),
-              onTap: () {
-                Navigator.pushNamed(context, '/sustainability');
-              },
-            ),
-          ],
-        ),
-      ) : null,
+            )
+          : null,
       body: child,
     );
   }
