a3a0e2c4b4b4dfa8c607774567b3e0e4fa41fad4
David Leon <da.leon@uniandes.edu.co>
2024-10-30 03:20:13 -0500

    Cange password view ahora es funcional con restricciones de formato comunes
    restricciones:
    Al menos 8 caracteres de longitud.
    Incluye al menos una letra mayúscula.
    Incluye al menos una letra minúscula.
    Incluye al menos un dígito.
    Incluye al menos un carácter especial (sin espacios).
    
    Se agrega mensaje de cambio de contraseña correcto
    
    se cambia el diseño de cambio de contraseña view para que sea mas consistente

--- ecostyle/lib/views/change_password_view.dart
+++ ecostyle/lib/views/change_password_view.dart
@@ -1,65 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class ChangePasswordView extends StatelessWidget {
+class ChangePasswordView extends StatefulWidget {
   const ChangePasswordView({super.key});
+
+  @override
+  _ChangePasswordViewState createState() => _ChangePasswordViewState();
+}
+
+class _ChangePasswordViewState extends State<ChangePasswordView> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _newPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController = TextEditingController();
+  String? _errorMessage;
+
+  bool _isPasswordValid(String password) {
+    // Validación de contraseña
+    final RegExp passwordRegex = RegExp(
+      r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
+    );
+    return passwordRegex.hasMatch(password);
+  }
+
+  Future<void> _changePassword() async {
+    try {
+      if (_newPasswordController.text != _confirmPasswordController.text) {
+        setState(() {
+          _errorMessage = 'Passwords do not match';
+        });
+        return;
+      }
+
+      if (!_isPasswordValid(_newPasswordController.text)) {
+        setState(() {
+          _errorMessage = 'Password must be at least 8 characters long, '
+              'include an uppercase letter, a lowercase letter, a digit, '
+              'and a special character without spaces.';
+        });
+        return;
+      }
+
+      User? user = _auth.currentUser;
+      if (user != null) {
+        await user.updatePassword(_newPasswordController.text);
+        // Mostrar mensaje de éxito
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Password changed successfully!')),
+        );
+        Navigator.pop(context); // Regresar a la vista de perfil después de cambiar la contraseña
+      }
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'Failed to change password: ${e.toString()}';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Change Password',
-          style: TextStyle(color: Colors.white), // Color del texto del título
-        ),
-        backgroundColor: const Color(0xFF012826),
-        iconTheme: const IconThemeData(color: Colors.white), // Color de los iconos
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              obscureText: true,
-              style: const TextStyle(color: Colors.white), // Color del texto ingresado
-              decoration: InputDecoration(
-                labelText: 'New Password',
-                labelStyle: const TextStyle(color: Colors.white), // Color de la etiqueta
-                filled: true,
-                fillColor: const Color(0xFF007451),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide.none,
-                ),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFFECECEC),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Change Password',
+                    style: const TextStyle(
+                      fontSize: 28,
+                      color: Color(0xFF012826),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  if (_errorMessage != null) ...[
+                    Text(
+                      _errorMessage!,
+                      style: const TextStyle(color: Colors.red),
+                    ),
+                    const SizedBox(height: 20),
+                  ],
+                  TextField(
+                    obscureText: true,
+                    controller: _newPasswordController,
+                    decoration: InputDecoration(
+                      hintText: 'New Password',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  TextField(
+                    obscureText: true,
+                    controller: _confirmPasswordController,
+                    decoration: InputDecoration(
+                      hintText: 'Confirm Password',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF007451),
+                        padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+                      ),
+                      onPressed: _changePassword, // Lógica para cambiar la contraseña
+                      child: const Text('Change Password', style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 20),
-            TextField(
-              obscureText: true,
-              style: const TextStyle(color: Colors.white), // Color del texto ingresado
-              decoration: InputDecoration(
-                labelText: 'Confirm Password',
-                labelStyle: const TextStyle(color: Colors.white), // Color de la etiqueta
-                filled: true,
-                fillColor: const Color(0xFF007451),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF007451),
-              ),
-              onPressed: () {
-                // Lógica para cambiar la contraseña
-                Navigator.pop(context); // Regresar a la vista de perfil después de cambiar la contraseña
-              },
-              child: const Text('Change Password', style: TextStyle(color: Colors.white)),
-            ),
-          ],
+          ),
         ),
       ),
     );
