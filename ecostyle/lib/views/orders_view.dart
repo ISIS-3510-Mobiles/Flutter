80bb45ba810ac49fe35306d9aff7db3b0da2c1f2
David Leon <da.leon@uniandes.edu.co>
2024-12-02 21:30:54 -0500

    implementa BQ y arreglos de EC

--- ecostyle/lib/views/orders_view.dart
+++ ecostyle/lib/views/orders_view.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class OrdersView extends StatefulWidget {
   @override
@@ -11,11 +12,43 @@ class OrdersView extends StatefulWidget {
 class _OrdersViewState extends State<OrdersView> {
   List<Map<String, dynamic>> orders = [];
   bool isLoading = true;
+  bool isConnected = true;
+
+  int firebaseLoadCounter = 0;
+  int cacheLoadCounter = 0;
 
   @override
   void initState() {
     super.initState();
+    _checkConnection();
     _loadOrders();
+  }
+
+  Future<void> _checkConnection() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {
+        isConnected = false;
+      });
+    } else {
+      setState(() {
+        isConnected = true;
+      });
+      // If connected, attempt to reload the data from Firebase
+      _loadOrders();
+    }
+    Connectivity().onConnectivityChanged.listen((connectivityResult) {
+      if (connectivityResult == ConnectivityResult.none) {
+        setState(() {
+          isConnected = false;
+        });
+      } else {
+        setState(() {
+          isConnected = true;
+        });
+        _loadOrders();
+      }
+    });
   }
 
   Future<void> _loadOrders() async {
@@ -23,6 +56,9 @@ class _OrdersViewState extends State<OrdersView> {
     String? cachedOrders = prefs.getString('cachedOrders');
 
     if (cachedOrders != null) {
+      // Increment cache load counter
+      cacheLoadCounter++;
+
       // Load cached data
       List<Map<String, dynamic>> cachedList = List<Map<String, dynamic>>.from(
         json.decode(cachedOrders).map((item) => Map<String, dynamic>.from(item)),
@@ -40,33 +76,42 @@ class _OrdersViewState extends State<OrdersView> {
         orders = cachedList;
         isLoading = false;
       });
+    } else {
+      setState(() {
+        isLoading = false;
+      });
     }
 
     try {
-      // Fetch data from Firebase
-      QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('orders').get();
+      if (isConnected) {
+        // Increment Firebase load counter
+        firebaseLoadCounter++;
 
-      List<Map<String, dynamic>> loadedOrders = querySnapshot.docs.map((doc) {
-        final data = doc.data() as Map<String, dynamic>;
-        return {
-          "orderDate": data['orderDate'] != null
-              ? (data['orderDate'] as Timestamp).toDate().toIso8601String()
-              : null,
-          "status": data['status'] ?? 'Confirmed',
-          "totalAmount": data['totalAmount'],
-          "userId": data['userId'],
-          "items": data['items'] ?? [],
-          "paymentMethod": data['paymentMethod'] ?? 'Not specified',
-        };
-      }).toList();
+        // Fetch data from Firebase
+        QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('orders').get();
 
-      // Cache data locally
-      await prefs.setString('cachedOrders', json.encode(loadedOrders));
+        List<Map<String, dynamic>> loadedOrders = querySnapshot.docs.map((doc) {
+          final data = doc.data() as Map<String, dynamic>;
+          return {
+            "orderDate": data['orderDate'] != null
+                ? (data['orderDate'] as Timestamp).toDate().toIso8601String()
+                : null,
+            "status": data['status'] ?? 'Confirmed',
+            "totalAmount": data['totalAmount'],
+            "userId": data['userId'],
+            "items": data['items'] ?? [],
+            "paymentMethod": data['paymentMethod'] ?? 'Not specified',
+          };
+        }).toList();
 
-      setState(() {
-        orders = loadedOrders;
-        isLoading = false;
-      });
+        // Cache data locally
+        await prefs.setString('cachedOrders', json.encode(loadedOrders));
+
+        setState(() {
+          orders = loadedOrders;
+          isLoading = false;
+        });
+      }
     } catch (e) {
       print('Error loading orders from Firebase: $e');
       setState(() {
@@ -95,84 +140,100 @@ class _OrdersViewState extends State<OrdersView> {
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: orders.isEmpty
-            ? const Center(child: Text('No orders found.'))
-            : ListView.builder(
-                itemCount: orders.length,
-                itemBuilder: (context, index) {
-                  final order = orders[index];
-                  return GestureDetector(
-                    onTap: () {
-                      // Navigate to the OrderDetailView and pass the order as an argument
-                      Navigator.pushNamed(
-                        context,
-                        '/orderDetail',
-                        arguments: order,
-                      );
-                    },
-                    child: Card(
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      margin: const EdgeInsets.symmetric(vertical: 8),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Order Date: ${order['orderDate'] ?? 'N/A'}'),
-                            Text('Total Amount: \$${order['totalAmount']}'),
-                            Text('Status: ${order['status']}'),
-                            Text('Payment Method: ${order['paymentMethod']}'),
-                            const SizedBox(height: 10),
-                            SingleChildScrollView(
-                              scrollDirection: Axis.horizontal,
-                              child: Row(
-                                children: List.generate(
-                                  order['items'].length,
-                                  (itemIndex) {
-                                    final item = order['items'][itemIndex];
-                                    return Padding(
-                                      padding: const EdgeInsets.only(right: 8.0),
-                                      child: Column(
-                                        children: [
-                                          Image.network(
-                                            item['image'] ?? '',
-                                            width: 80,
-                                            height: 80,
-                                            fit: BoxFit.cover,
-                                          ),
-                                          const SizedBox(height: 5),
-                                          SizedBox(
-                                            width: 80,
-                                            child: Text(
-                                              item['title'] ?? 'No title',
-                                              style: const TextStyle(fontSize: 12),
-                                              overflow: TextOverflow.ellipsis,
-                                              maxLines: 1,
+      body: Column(
+        children: [
+          if (!isConnected)
+            Container(
+              color: Colors.red,
+              padding: const EdgeInsets.all(8.0),
+              child: const Text(
+                'No internet connection. Displaying cached data if available.',
+                style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: orders.isEmpty
+                  ? const Center(child: Text('No orders found.'))
+                  : ListView.builder(
+                      itemCount: orders.length,
+                      itemBuilder: (context, index) {
+                        final order = orders[index];
+                        return GestureDetector(
+                          onTap: () {
+                            // Navigate to the OrderDetailView and pass the order as an argument
+                            Navigator.pushNamed(
+                              context,
+                              '/orderDetail',
+                              arguments: order,
+                            );
+                          },
+                          child: Card(
+                            elevation: 4,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            margin: const EdgeInsets.symmetric(vertical: 8),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Order Date: ${order['orderDate'] ?? 'N/A'}'),
+                                  Text('Total Amount: \$${order['totalAmount']}'),
+                                  Text('Status: ${order['status']}'),
+                                  Text('Payment Method: ${order['paymentMethod']}'),
+                                  const SizedBox(height: 10),
+                                  SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: Row(
+                                      children: List.generate(
+                                        order['items'].length,
+                                        (itemIndex) {
+                                          final item = order['items'][itemIndex];
+                                          return Padding(
+                                            padding: const EdgeInsets.only(right: 8.0),
+                                            child: Column(
+                                              children: [
+                                                Image.network(
+                                                  item['image'] ?? '',
+                                                  width: 80,
+                                                  height: 80,
+                                                  fit: BoxFit.cover,
+                                                ),
+                                                const SizedBox(height: 5),
+                                                SizedBox(
+                                                  width: 80,
+                                                  child: Text(
+                                                    item['title'] ?? 'No title',
+                                                    style: const TextStyle(fontSize: 12),
+                                                    overflow: TextOverflow.ellipsis,
+                                                    maxLines: 1,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  '\$${item['price']}',
+                                                  style: const TextStyle(fontSize: 12),
+                                                ),
+                                              ],
                                             ),
-                                          ),
-                                          Text(
-                                            '\$${item['price']}',
-                                            style: const TextStyle(fontSize: 12),
-                                          ),
-                                        ],
+                                          );
+                                        },
                                       ),
-                                    );
-                                  },
-                                ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                          ],
-                        ),
-                      ),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
+            ),
+          ),
+        ],
       ),
     );
   }
